c52382f4dc5afb1ff3f34cba0bc012cfd64a386a
saorio <saori@SaorinoMacBook-Air.local>
2017-11-26 17:31:30 -0800

    add interface.go

---
+++ interface.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// interface
+type Car interface {
+	run(int) string
+	stop()
+}
+
+// structure
+type MyCar struct {
+	name  string
+	speed int
+}
+
+// method
+func (u *MyCar) run(speed int) string {
+	u.speed = speed
+	return strconv.Itoa(speed) + "kmで走ります"
+}
+
+func (u *MyCar) stop() {
+	fmt.Println("停止します")
+	u.speed = 0
+}
+
+func main() {
+	myCar := &MyCar{name: "マイカー", speed: 0}
+
+	var i Car = myCar
+	fmt.Println(i.run(50))
+	i.stop()
+}
