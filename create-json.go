76536a7f9cfcf57d77a8f1f1c7ada6cdf8884e5d
saorio <saori@SaorinoMacBook-Air.local>
2017-11-26 17:09:19 -0800

    add create-json.go

---
+++ create-json.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Department struct {
+	Name   string    `json:"name"`
+	Member []Profile `json:"profile"`
+}
+
+type Profile struct {
+	Name string `json:"name"`
+	Age  string `json:"age"`
+	Sex  string `json:sex"`
+}
+
+func main() {
+	saori := Profile{Name: "saori", Age: "27", Sex: "female"}
+	norikazu := Profile{Name: "norikazu", Age: "31", Sex: "male"}
+	yosuke := Profile{Name: "yosuke", Age: "32", Sex: "male"}
+	development := Department{
+		Name:   "開発部",
+		Member: []Profile{saori, norikazu, yosuke},
+	}
+	jsonBytes, err := json.Marshal(development)
+	if err != nil {
+		fmt.Println("JSON Mershal error:", err)
+		return
+	}
+	fmt.Println(string(jsonBytes))
+}
